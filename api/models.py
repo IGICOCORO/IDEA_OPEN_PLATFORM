57ecb90d10c738ae781ca80d02aa5d325c3ed20c
IGICOCORO <nduwimanaguymich@gmail.com>
2022-09-23 15:24:48 +0200

    removed taggit module

--- api/models.py
+++ api/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 # Create your models here.
-
 class Tags(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
     name = models.CharField(max_length=50, unique=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_tags")
 
+    def __str__(self):
+        return f"{self.name} {self.created_by}"
+
 class Posts(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
     name = models.CharField(max_length=50)
     content = models.TextField(max_length=1000)
+    is_liked = models.BooleanField()
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     tags = models.ManyToManyField('Tags', related_name='posts')
-    is_liked = models.BooleanField()
 
-    def __str__():
-    	return f"{self.content} {self.tags}"
+    def __str__(self):
+        return f"{self.name} {self.content} {self.created_by}"
 
 class Comment(models.Model):
     post = models.ForeignKey(Posts, on_delete=models.CASCADE)
